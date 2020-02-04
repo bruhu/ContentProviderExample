9c9ff1de4eb424a8a6160ee1c388d3bd417ce4f0
bruhu <annabru.abh@gmail.com>
2020-02-04 11:28:44 +0100

    Added do while statement to loop through cursor values, getting the names and using getColumnIndex since we don't know the index number

--- app/src/main/java/bruhu/contentproviderexample/com/MainActivity.kt
+++ app/src/main/java/bruhu/contentproviderexample/com/MainActivity.kt
@@ -17,7 +17,13 @@ class MainActivity : AppCompatActivity() {
         val cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null)
         // check if cursor is not null and if so, point to the first item
         if(cursor != null && cursor.moveToFirst()){
-            // use do while looping the cursor until it has next value
+            // use do while looping the cursor until it has next value, so we can read all the values from the cursor
+            do {
+                // using cursor trying to get the name - we use getColumnIndex since we don't know the index number
+                val name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                // Logging the name value
+
+            }while(cursor.moveToNext())
         }
     }
 }
