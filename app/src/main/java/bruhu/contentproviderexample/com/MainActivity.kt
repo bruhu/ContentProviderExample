9bcea4778523b3a93be29c93403e23fab59b034f
bruhu <annabru.abh@gmail.com>
2020-02-04 11:23:09 +0100

    MainActivity -> added val cursor with the query to access the contacts

--- app/src/main/java/bruhu/contentproviderexample/com/MainActivity.kt
+++ app/src/main/java/bruhu/contentproviderexample/com/MainActivity.kt
@@ -2,11 +2,18 @@ package bruhu.contentproviderexample.com
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun getContacts(){
+        // this will keep the data that we will later request
+        // CONTENT_URI has the URI to access the contacts
+        val cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null)
     }
 }
